f5c0dfb6535f479f0d0057d1be26f7e50105573d
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-28 02:58:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-let kittens = ["Milo", "Otis", "Garfield"]
+const kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
   kittens.push("Ralph");
